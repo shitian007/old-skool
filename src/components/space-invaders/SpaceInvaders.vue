0ce917ec615ba11cfc9fb8a6f79057a5dd422233
Shi Tian <shitian007@hotmail.com>
2017-09-30 16:27:18 +0800

    Setup Space Invaders files

---
+++ src/components/space-invaders/SpaceInvaders.vue
@@ -0,0 +1,8 @@
+<template src="./space-invaders.html">
+</template>
+
+<script src="./space-invaders.js">
+</script>
+
+<style src="./space-invaders.scss">
+</style>
---
+++ src/components/space-invaders/space-invaders.js
@@ -0,0 +1,7 @@
+export default {
+  name: 'space-invaders',
+  data() {
+    return {
+    };
+  }
+};
