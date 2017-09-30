bbcee6fd16437179a24e1fa0dae4d9a1e7b13558
Shi Tian <shitian007@hotmail.com>
2017-09-30 15:17:07 +0800

    Setup Homepage

---
+++ src/components/home/Home.vue
@@ -0,0 +1,9 @@
+<template src="./home.html">
+</template>
+
+<script src="./home.js">
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./home.scss">
+</style>
